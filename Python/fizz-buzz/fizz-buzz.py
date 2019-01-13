245af729847057b32d0723ad1de1610488acd688
Philip Wales <me@ptwales.net>
2019-01-12 20:33:33 -0600

    python fizz buzz

---
+++ Python/fizz-buzz/fizz-buzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+for n in range(1, 100):
+    if n % 15 == 0:
+        print("fizz buzz")
+    elif n % 3 == 0:
+        print("fizz")
+    elif n % 5 == 0:
+        print("buzz")
+    else:
+        print(n)
+
