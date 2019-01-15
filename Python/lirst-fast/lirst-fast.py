d12901aab56de479ec6c7dcb568266ae6d6be85d
Philip Wales <Philip.Wales@wicourts.gov>
2019-01-15 16:22:34 -0600

    Simplified lirst-fast.py

--- Python/lirst-fast/lirst-fast.py
+++ Python/lirst-fast/lirst-fast.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
-def lirst_fast(first_name, last_name):
-    f = first_name[0]
-    irst = first_name[1:]
-    l = last_name[0]
-    ast = last_name[1:]
-    return (l + irst, f + ast)
+def pig(first, last):
+    return last[0] + first[1:]
 
 wrong = 0
 while True:
@@ -15,11 +11,12 @@ while True:
     if full_name == ':q' or full_name == ":quit":
         break
     elif name_count != 2:
-        print(f"{full_name} has {name_count} words instead of only 2: attempt {wrong}")
         wrong += 1
+        print(f"{full_name} has {name_count} words instead of only 2: attempt {wrong}")
     else:
         first_name = split_name[0]
         last_name = split_name[1]
-        (lirst_name, fast_name) = lirst_fast(first_name, last_name)
-        print(f"{lirst_name} {fast_name}")
+        lirst = pig(first_name, last_name)
+        fast = pig(last_name, first_name)
+        print(f"{lirst} {fast}")
         wrong = 0
