49f76f85db9c86e84e0ceb7f390a23620eb647df
Philip Wales <Philip.Wales@wicourts.gov>
2019-01-16 15:05:52 -0600

    More functional answer using streams and tailrec

--- Scala/lirst-fast/LirstFast.scala
+++ Scala/lirst-fast/LirstFast.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import scala.io.StdIn.readLine
 
 object Main extends App {
@@ -5,24 +6,37 @@ object Main extends App {
   def pig(first: String, last: String): String = 
     (last.head +: first.tail).mkString
 
-  def prompt: String = 
+  def prompt: List[String] = 
     readLine("What is your full name? ").split(" ").toList
 
-  var wrong: Int = 0
-  while (true) {
+  final case class State(quit: Boolean, wrong: Int)
+
+  def handle(state: State): State = 
     prompt match {
       case first :: last :: Nil =>
         val lirst = pig(first, last)
         val fast = pig(last, first)
         println(s"$lirst $fast")
+        State(false, 0)
       case ":q" :: Nil | ":quit" :: Nil =>
-        sys.exit(0)
+        State(true, state.wrong)
       case badName@_ => 
-        val name = badName.mkString
+        val name = badName.mkString(" ")
         val wordCount = badName.size
-        wrong += 1
-        println(s"$name has $wordCount words instead of only 2: attempt ${wrong}")
+        val wrongInc = state.wrong + 1
+        println(s"$name has $wordCount words instead of only 2: attempt ${wrongInc}")
+        State(false, wrongInc)
     }
-  }
 
+  @tailrec
+  def run(prev: State, stream: Stream[State => State]): State =
+    stream match {
+      case h #:: hs =>
+        val next = h(prev)
+        if (!next.quit) run(next, hs)
+        else next
+      case Stream.Empty => prev
+    }
+
+  run(State(false, 0), Stream.continually(handle(_)))
 }
