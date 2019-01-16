22bb8af516a1a363b615b6fa87342bf42e36d27f
Philip Wales <Philip.Wales@wicourts.gov>
2019-01-16 14:26:37 -0600

    Even simpler scala LisrtFast

--- Scala/lirst-fast/LirstFast.scala
+++ Scala/lirst-fast/LirstFast.scala
@@ -5,37 +5,23 @@ object Main extends App {
   def pig(first: String, last: String): String = 
     (last.head +: first.tail).mkString
 
-  sealed trait Input
-  case class Good(first: String, last: String) extends Input
-  case class Wrong(badName: List[String]) extends Input
-  case object Quit extends Input
-
-  def prompt: Input = {
-    val words = readLine("What is your full name? ").split(" ").toList
-    words match {
-      case first :: last :: Nil =>
-        Good(first, last)
-      case ":q" :: Nil | ":quit" :: Nil =>
-        Quit
-      case badName@_ => 
-        Wrong(badName)
-    }
-  }
+  def prompt: String = 
+    readLine("What is your full name? ").split(" ").toList
 
   var wrong: Int = 0
   while (true) {
     prompt match {
-      case Good(first, last) =>
+      case first :: last :: Nil =>
         val lirst = pig(first, last)
         val fast = pig(last, first)
         println(s"$lirst $fast")
-      case Wrong(badName) =>
+      case ":q" :: Nil | ":quit" :: Nil =>
+        sys.exit(0)
+      case badName@_ => 
         val name = badName.mkString
         val wordCount = badName.size
         wrong += 1
         println(s"$name has $wordCount words instead of only 2: attempt ${wrong}")
-      case Quit => 
-        sys.exit(0)
     }
   }
 
