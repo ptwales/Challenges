d8c657d83b09f34a0c939c0f91a02f71b78c61b5
Philip Wales <Philip.Wales@wicourts.gov>
2019-01-22 16:16:48 -0600

    remove trailing space from C echo

--- C/core-utils/echo.c
+++ C/core-utils/echo.c
@@ -1,8 +1,12 @@
 #include "stdio.h"
 
 int main(int argc, char *argv[]) {
-    for (int i = 1; i < argc; ++i) {
+    int last = argc - 1;
+    for (int i = 1; i < last; ++i) {
         printf("%s ", argv[i]);
+    }
+    if (last > 0) {
+        printf("%s", argv[last]);
     }
     printf("\n");
 }
